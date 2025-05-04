f0e4c044e20f1b2bac6916c9953c05634413c99d
Markus Stenberg <markus.stenberg@iki.fi>
2025-05-04 20:29:40 +0300

    utils: Precompile also underscore removal

--- utils.go
+++ utils.go
@@ -8,10 +8,12 @@ import (
 // sanitizeFilename removes characters that are problematic in filenames/paths.
 var sanitizeRegex = regexp.MustCompile(`[<>:"/\\|?*\x00-\x1F#]`)
 
+// Remove more than one _
+var multiUnderscoreRegex = regexp.MustCompile(`__+`)
+
 func sanitizeFilename(name string) string {
 	sanitized := sanitizeRegex.ReplaceAllString(name, "_")
-	// Replace multiple underscores with a single one
-	sanitized = regexp.MustCompile(`_+`).ReplaceAllString(sanitized, "_")
+	sanitized = multiUnderscoreRegex.ReplaceAllString(sanitized, "_")
 	// Trim leading/trailing underscores/spaces/dots
 	sanitized = strings.Trim(sanitized, "_ .")
 	if sanitized == "" {
